9095c03adfbcc74de22037fc71c70dc5f20c20eb
Darx <snehankobose31@gmail.com>
2022-12-03 01:12:13 +0530

    api integration added but failing due to invalid data

--- src/component/weather/Weather.js
+++ src/component/weather/Weather.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import './weather.scss';
 
+const APIkey= '895b84687fcddc3e0be9289362976c63';
+
 function Weather() {
 
   const [weatherData,setWeatherData]=useState();
@@ -10,8 +12,7 @@ function Weather() {
     city:'Durgapur',
     country:'India'
   })
-
-  const APIkey= '895b84687fcddc3e0be9289362976c63';
+  
   const URL=`https://api.openweathermap.org/data/2.5/weather?q=${cityData.city},${cityData.country}&units=metric&appid=${APIkey}`;
 
   const handleChange=(e)=>{
@@ -19,46 +20,48 @@ function Weather() {
   }
 
   const handleClick=()=>{
-      axios.get(URL)
-      .then((response)=>{
-        setWeatherData(response.data);
-        console.log(weatherData);
-      })
-      .catch((err)=>console.log(err));
-  }
+        axios.get(URL)
+        .then((response)=>{
+          setWeatherData(response.data);    
+          
+        })
+        .catch((err)=>
+        console.log(err))
+    }
 
-  useEffect(()=>{
-      
-      handleClick();
+  useEffect(()=>{    
+    handleClick();
   },[])
+
+  
   
   //const iconURL=`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
 
-  return (
-      <div className='container'>
-        <div className='search-container'>
-              <div className="input-container">
-                <input name="city" className='city-input' type='text' placeholder='Enter City Name' onChange={handleChange} />
-                <input name="country" className='country-input' type='text' placeholder='Enter Country Name'onChange={handleChange}/>
-              </div>            
-              <button className="btn search-btn" onClick={handleClick}>Search</button>
-        </div>
-        <div className='weather-container'>
-            <div className='city-container'>
-              <h1>London, GB</h1>
-              <h3>Thrusday, March 21, 2021</h3>
-            </div>
-            <div className='temp-container'>
-              <h1>5'C</h1>
-              <div>Image/icon</div>
-              <h2>Detailed Weather</h2>
-            </div>
-            <div className='info-container'>
-              <h2>Humidity: 87%</h2>
-              <h2>Pressure: 1020hPa</h2>
-            </div>
-        </div>     
-      </div>   
+  return ( 
+            <div className='container'>
+              <div className='search-container'>
+                    <div className="input-container">
+                      <input name="city" className='city-input' type='text' placeholder='Enter City Name' value={cityData.city} onChange={handleChange} />
+                      <input name="country" className='country-input' type='text' placeholder='Enter Country Name'value={cityData.country} onChange={handleChange}/>
+                    </div>            
+                    <button className="btn search-btn" onClick={handleClick}>Search</button>
+              </div>
+              <div className='weather-container'>
+                  <div className='city-container'>
+                    <h1>{weatherData.name},{weatherData.sys.country}</h1>
+                    <h3>Thrusday, March 21, 2021</h3>
+                  </div>
+                  <div className='temp-container'>
+                    <h1>{weatherData.main.temp}'C</h1>
+                    {/* <img src={iconURL}/> */}
+                    <h2>Detailed Weather</h2>
+                  </div>
+                  <div className='info-container'>
+                    <h2>Humidity: 87%</h2>
+                    <h2>Pressure: 1020hPa</h2>
+                  </div>
+              </div>  
+            </div>      
   )
 }
 
